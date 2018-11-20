eae9614ef0dce69f8bfc32dad4fae469353a9895
Nick Samoylov <nick.samoylov@ctl.io>
2018-11-19 19:51:59 -0700

    Interface examples added

--- src/main/java/com/packt/learnjava/ch02_oop/InterfaceExamples.java
+++ src/main/java/com/packt/learnjava/ch02_oop/InterfaceExamples.java
@@ -7,9 +7,14 @@ public class InterfaceExamples {
     public static void main(String... args){
 
         staticProperty();
-
         SomeInterface si = new SomeClass();
 
+        SomeClass sc = new SomeClass();
+        sc.method1();
+        sc.method2(22);  //returns: "abc"
+        sc.method3();       //returns: 42
+
+        System.out.println(SomeInterface1.someMethod());
     }
 
     private static class SomeClass implements SomeInterface{
@@ -20,12 +25,20 @@ public class InterfaceExamples {
             //method body
             return "abc";
         }
-
     }
 
     interface SomeInterface {
         void method1();
         String method2(int i);
+        default int method3(){
+            return getNumber();
+        }
+        default int method4(){
+            return getNumber() + 22;
+        }
+        private int getNumber(){
+            return 42;
+        }
     }
 
     private static void staticProperty(){
@@ -36,6 +49,12 @@ public class InterfaceExamples {
 
     interface A {
         Set<String> set = new HashSet<>();
+    }
+
+    interface SomeInterface1{
+        static String someMethod() {
+            return "abc";
+        }
     }
 
 }
