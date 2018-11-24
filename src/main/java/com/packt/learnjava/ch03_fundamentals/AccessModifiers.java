b1ec34cd6ed5054bb7c376951bc2506d26dc9311
Nick Samoylov <nick.samoylov@ctl.io>
2018-11-24 15:41:41 -0700

    Add examples of access modifiers and inner classes

--- src/main/java/com/packt/learnjava/ch03_fundamentals/AccessModifiers.java
+++ src/main/java/com/packt/learnjava/ch03_fundamentals/AccessModifiers.java
@@ -20,4 +20,39 @@ public class AccessModifiers {
     protected interface I2 {}
     private interface I3 {}
     public interface I4 {}
+
+
+    private String topLevelPrivateProperty = "Top-level private value";
+    private void topLevelPrivateMethod(){
+        var inner = new InnerClass();
+        System.out.println(inner.innerPrivateProperty);
+        inner.innerPrivateMethod();
+    }
+
+    private class InnerClass {
+        //private static String PROP = "Inner static"; //error
+        private String innerPrivateProperty = "Inner private value";
+        private void innerPrivateMethod(){
+            System.out.println(topLevelPrivateProperty);
+        }
+    }
+
+    private static class InnerStaticClass {
+        private static String PROP = "Inner private static";
+        private String innerPrivateProperty = "Inner private value";
+        private void innerPrivateMethod(){
+            var top = new AccessModifiers();
+            System.out.println(top.topLevelPrivateProperty);
+        }
+    }
+
+    public static void main(String... args){
+        var top = new AccessModifiers();
+        top.topLevelPrivateMethod();
+        //var inner = new InnerClass();  //error
+        System.out.println(InnerStaticClass.PROP);
+        var inner = new InnerStaticClass();
+        System.out.println(inner.innerPrivateProperty);
+        inner.innerPrivateMethod();
+    }
 }
