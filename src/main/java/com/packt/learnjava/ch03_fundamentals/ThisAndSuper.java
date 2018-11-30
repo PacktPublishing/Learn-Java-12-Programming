d61d4c61f40551c69271475bef1b410decae5fcb
Nick Samoylov <nick.samoylov@ctl.io>
2018-11-29 19:37:19 -0700

    Example of this keyword is added

--- src/main/java/com/packt/learnjava/ch03_fundamentals/ThisAndSuper.java
+++ src/main/java/com/packt/learnjava/ch03_fundamentals/ThisAndSuper.java
@@ -1,12 +1,19 @@
 package com.packt.learnjava.ch03_fundamentals;
 
 public class ThisAndSuper {
+    public static void main(String... args){
+        A a = new A();
+        a.setCount(2);
+        System.out.println(a.getCount()); //prints: 0
+    }
 
     private static class A {
         private int count;
         public void setCount(int count) {
             count = count;
-            //do some calculations
+        }
+        public int getCount(){
+            return this.count;
         }
     }
 }
