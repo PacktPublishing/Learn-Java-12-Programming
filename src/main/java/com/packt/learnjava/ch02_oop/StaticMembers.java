8d9f16d79d91c786d5f0e863e7fad46f72fda621
Nick Samoylov <nick.samoylov@ctl.io>
2018-12-01 15:45:42 -0700

    Examples for boxing and unboxing are added

--- src/main/java/com/packt/learnjava/ch02_oop/StaticMembers.java
+++ src/main/java/com/packt/learnjava/ch02_oop/StaticMembers.java
@@ -4,13 +4,13 @@ public class StaticMembers {
     public static void main(String... args){
 
         SomeClass.someMethod(42);
-        System.out.println(SomeClass.somePrpperty);  //prints: abc
+        System.out.println(SomeClass.SOME_PROPERTY);  //prints: abc
 
     }
 
-    private static class SomeClass{
+    public static class SomeClass{
         public static final String INPUT_FILE_NAME = "myFile.csv";
-        public static String somePrpperty = "abc";
+        public static String SOME_PROPERTY = "abc";
         public static void someMethod(int i){
             //do something
         }
