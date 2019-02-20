2956c9f79fb526b5f21246ae9867bc5abc550eff
Nick Samoylov <nick.samoylov@ctl.io>
2019-02-19 21:04:16 -0700

    Add functional programming examples

---
+++ src/main/java/com/packt/learnjava/ch13_functional/LambdaExpressions.java
@@ -0,0 +1,51 @@
+package com.packt.learnjava.ch13_functional;
+
+import java.util.function.Consumer;
+
+public class LambdaExpressions {
+    public static void main(String... args) {
+        introduction();
+        thisdDemo();
+
+    }
+
+    private static void introduction(){
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Hello!");
+            }
+        };
+        runnable.run();   //prints: Hello!
+
+        Runnable runnable2 = () -> System.out.println("Hello!");
+        runnable2.run();
+    }
+
+
+    private static void thisdDemo(){
+        ThisDemo d = new ThisDemo();
+        d.useAnonymousClass();   //prints: Consumer.field
+        d.useLambdaExpression(); //prints: ThisDemo.field
+    }
+
+    private static class ThisDemo {
+        private String field = "ThisDemo.field";
+        public void useAnonymousClass() {
+            Consumer<String> consumer = new Consumer<>() {
+                private String field = "Consumer.field";
+                public void accept(String s) {
+                    System.out.println(this.field);
+                }
+            };
+            consumer.accept(this.field);
+        }
+        public void useLambdaExpression() {
+            Consumer<String> consumer = consumer = s -> {
+                System.out.println(this.field);
+            };
+            consumer.accept(this.field);
+        }
+    }
+}
